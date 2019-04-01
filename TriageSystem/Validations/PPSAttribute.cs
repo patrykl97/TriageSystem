2e2e93c4165e70951a61d09bcbd136261493502a
Patryk Leszczynski <patryk.leszczynski1997@gmail.com>
2019-04-01 05:27:21 +0100

    Added PPS validation

---
+++ TriageSystem/Validations/PPSAttribute.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace TriageSystem.Validations
+{
+    public class PPSAttribute : ValidationAttribute
+    {
+
+
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            if (value != null)
+            {
+                string pps = value.ToString();
+                string regex = "[0-9]{7}[A-Za-z]{1}[AaHhWw]?";
+                if (!Regex.IsMatch(pps, regex, RegexOptions.IgnoreCase))
+                {
+                    return new ValidationResult(GetErrorMessage());
+                }
+            }
+
+            return ValidationResult.Success;
+        }
+
+        private string GetErrorMessage()
+        {
+            return "PPS number is invalid!";
+        }
+    }
+
+}
--- TriageSystem/ViewModels/PatientCheckInViewModel.cs
+++ TriageSystem/ViewModels/PatientCheckInViewModel.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
+using TriageSystem.Validations;
 
 namespace TriageSystem.ViewModels
 {
@@ -11,6 +12,7 @@ namespace TriageSystem.ViewModels
     {
         public int PatientId { get; set; }
         //[Required]
+        [PPSAttribute]
         public string PPS { get; set; }
         [Required]
         public string Full_name { get; set; }
